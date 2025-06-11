0cb9c7c1a01f4cc2d8b8910a275b26b2b2d60e66
LeandroBryto <leandrobarreto.barreto321@gmail.com>
2025-06-11 20:47:01 -0300

    feat:(model) Cronogrma
    criando entidade cronograma

---
+++ src/main/java/leandro/dev/gestao_obras/model/Cronograma.java
@@ -0,0 +1,52 @@
+package leandro.dev.gestao_obras.model;
+
+import jakarta.persistence.*;
+import leandro.dev.gestao_obras.enums.StatusCronograma;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "cronogramas")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Cronograma {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToMany(fetch = FetchType.LAZY)
+    @JoinColumn(name = "obra_id", nullable = false, unique = true)
+    private Obra obra;
+
+    @Column(name = "data_inicio_projeto",nullable = false)
+    private LocalDate dataInicioProjeto;
+
+    @Column(name = "data_termino_prevista", nullable = false)
+    private LocalDate dataTerminoPrevista;
+
+    @Column(name = "data_termino_atual") // Pode ser nulo inicialmente
+    private LocalDate dataTerminoAtuL;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "status_geral")
+    private StatusCronograma statusGeral;
+
+    @Column(name = "dias_atraso_adiantamento")
+    private Integer diasAtrasoAdiantamento;    // Positivo para adiantamento , negativo para atraso
+
+    @ElementCollection(fetch = FetchType.LAZY)
+    @CollectionTable(name = "cronograma_marcos", joinColumns = @JoinColumn(name = "cronograma_id"))
+    @Column(name = "marco")
+    private List<String> marcosImportantes = new ArrayList<>();
+
+    @Lob
+    private String registraAtrasos;
+
+}
