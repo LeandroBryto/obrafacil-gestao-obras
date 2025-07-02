c928ea40eeb4ff6d9abe595b0c06fd1da1094d6a
LeandroBryto <leandrobarreto.barreto321@gmail.com>
2025-07-02 12:09:07 -0300

    feat:(ontroller)DiarioObraController
    adicionado endpoint Post para criação de registros no diário de obra

---
+++ src/main/java/leandro/dev/gestao_obras/controller/DiarioObraController.java
@@ -0,0 +1,70 @@
+package leandro.dev.gestao_obras.controller;
+
+import leandro.dev.gestao_obras.model.DiarioObra;
+import leandro.dev.gestao_obras.model.Etapa;
+import leandro.dev.gestao_obras.model.Obra;
+import leandro.dev.gestao_obras.repository.DiarioObraRepository;
+import leandro.dev.gestao_obras.repository.EtapaRepository;
+import leandro.dev.gestao_obras.repository.ObraRepository;
+import org.hibernate.annotations.Parameter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api")
+public class DiarioObraController {
+
+    @Autowired
+    private DiarioObraRepository diarioObraRepository;
+
+    @Autowired
+    private ObraRepository obraRepository;
+
+    @Autowired
+    private EtapaRepository etapaRepository;
+
+    // Endpoint para criar um novo registro no diario de obra
+    @PostMapping("obras/{obraId}/diario")
+    private ResponseEntity<DiarioObra> criarRegistroDiario(@PathVariable Long obraId, @RequestBody DiarioObra diarioObra){
+        Optional<Obra> obraData = obraRepository.findById(obraId);
+        if (obraData.isEmpty() || obraData.get().isArquivado()){
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        try {
+            diarioObra.setObra(obraData.get());
+            if (diarioObra.getDataHora() == null){
+                diarioObra.setDataHora(LocalDateTime.now());
+            }
+            if (diarioObra.getEtapaRelacionada() != null && diarioObra.getEtapaRelacionada().getId() !=null){
+                Optional<Etapa> etapaData = etapaRepository.findById(diarioObra.getEtapaRelacionada().getId());
+                if (obraData.isPresent() && etapaData.get().getObra().getId().equals(obraId)){
+                    diarioObra.setEtapaRelacionada(etapaData.get());
+                }else {
+                    diarioObra.setEtapaRelacionada(null);
+                }
+            }else {
+                diarioObra.setEtapaRelacionada(null);
+            }
+            // salva o registro
+            DiarioObra novoRegistro = diarioObraRepository.save(diarioObra);
+
+            // verificar se é critco e dispara notoficação (placeholder)
+            if (novoRegistro.isCritico()){
+                System.out.println("ALERTA: Ocorrência crítica registrada! ID:" + "obra:" + obraData.get().getNome());
+                // TODO:Implementar envio real de notificação (email, SMS, ETC. )
+            }
+            return new ResponseEntity<>(novoRegistro, HttpStatus.CREATED);
+        }catch (Exception e){
+            System.out.println("Erro ao criar registra no diario:" + e.getMessage());
+            return new ResponseEntity<>(null,HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
+    }
+
+}
