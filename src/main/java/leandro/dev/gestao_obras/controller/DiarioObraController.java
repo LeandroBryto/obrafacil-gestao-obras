96bce6c63df5841490bbcf3d927bdb8b42f33057
LeandroBryto <leandrobarreto.barreto321@gmail.com>
2025-07-04 22:00:48 -0300

    feat:(controller)DiarioObraControllerr
    Implementado endpoint GET , listagem com filtros

--- src/main/java/leandro/dev/gestao_obras/controller/DiarioObraController.java
+++ src/main/java/leandro/dev/gestao_obras/controller/DiarioObraController.java
@@ -9,6 +9,7 @@ import leandro.dev.gestao_obras.repository.EtapaRepository;
 import leandro.dev.gestao_obras.repository.ObraRepository;
 import org.hibernate.annotations.Parameter;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
@@ -70,6 +71,31 @@ public class DiarioObraController {
         }
 
     }
+    // Enpoint para listar registro do diario de obra com filtro
+    @GetMapping("/obras/{obraId}/diario")
+    public ResponseEntity<List<DiarioObra>> listarRegistrosDiario(
+            @PathVariable Long obraId,
+            @RequestParam(required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate dataIncio,
+            @RequestParam(required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate dataFim,
+            @RequestParam(required = false) TipoRegistroDiario tipo,
+            @RequestParam(required = false) Long etapaId){
+        Optional<Obra> obraData = obraRepository.findById(obraId);
+        if (obraData.isEmpty() || obraData.get().isArquivado()){
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        try {
+            Specification<DiarioObra> spec = criarEspecificacaoFiltro(obraId,dataIncio,dataFim,tipo,etapaId);
+            List<DiarioObra> registro = diarioObraRepository.findById(spec);
+
+            if (registro.isEmpty()){
+                return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+            }
+            return new ResponseEntity<>(registro, HttpStatus.OK);
+        }catch (Exception e ){
+            System.out.println("Erro ao listar registro do diario:" + e.getMessage());
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
     // Endpoint para buscar uma registro especifico do diario porID
     @GetMapping("/obras/{obraId}/diario/relatorio")
     public ResponseEntity<DiarioObra> buscarRegistroDiarioPorId(@PathVariable Long diarioId){
@@ -80,5 +106,6 @@ public class DiarioObraController {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
     }
+   
 
 }
