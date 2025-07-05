93ac4d1657eb2e9e7bbc22f185b14cc347420953
LeandroBryto <leandrobarreto.barreto321@gmail.com>
2025-07-05 17:23:54 -0300

    feat:(repository)CronogramaRepository
    adicionado método para buscar cronograma por ObraId no repositório

--- src/main/java/leandro/dev/gestao_obras/repository/CronogramaRepository.java
+++ src/main/java/leandro/dev/gestao_obras/repository/CronogramaRepository.java
@@ -1,10 +1,15 @@
 package leandro.dev.gestao_obras.repository;
 
+import leandro.dev.gestao_obras.model.Cronograma;
 import leandro.dev.gestao_obras.model.Obra;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
-public interface CronogramaRepository extends JpaRepository<Obra , Long> {
-    
+public interface CronogramaRepository extends JpaRepository<Cronograma , Long> {
+    Optional<Cronograma> findByObraId(Long obraId);
+    List<Cronograma> findByStatus(String status);
 }
