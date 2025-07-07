a3cf7c03e5f1cdf5ade5a4b9bcdd65d969e08dd7
LeandroBryto <leandrobarreto.barreto321@gmail.com>
2025-07-07 10:53:53 -0300

    refactor:(repository)MarcoCronogramaRepositoty
    corrigido: tipo da entidade e método

--- src/main/java/leandro/dev/gestao_obras/repository/MarcoCronogramaRepository.java
+++ src/main/java/leandro/dev/gestao_obras/repository/MarcoCronogramaRepository.java
@@ -1,13 +1,14 @@
 package leandro.dev.gestao_obras.repository;
 
 import leandro.dev.gestao_obras.model.Cronograma;
+import leandro.dev.gestao_obras.model.MarcoCronograma;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
-public interface MarcoCronogramaRepository extends JpaRepository<Cronograma , Long> {
+public interface MarcoCronogramaRepository extends JpaRepository<MarcoCronograma , Long> {
     // Busca o cronograma associado a uma obra especifica
-    Optional<Cronograma> findByObraId(Long obraId);
+    Optional<MarcoCronograma> findByCronogramaId(Long cronogramaId);
 }
