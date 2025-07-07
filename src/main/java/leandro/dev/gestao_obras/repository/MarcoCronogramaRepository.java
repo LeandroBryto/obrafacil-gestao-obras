e81617acdd90b9fcd477b6de1cbc4c47cb8ef983
LeandroBryto <leandrobarreto.barreto321@gmail.com>
2025-07-07 12:20:24 -0300

    feat:(repository)MarcoCronogramaRepository
    refactor : corrigir repositório

--- src/main/java/leandro/dev/gestao_obras/repository/MarcoCronogramaRepository.java
+++ src/main/java/leandro/dev/gestao_obras/repository/MarcoCronogramaRepository.java
@@ -1,14 +1,13 @@
 package leandro.dev.gestao_obras.repository;
 
-import leandro.dev.gestao_obras.model.Cronograma;
 import leandro.dev.gestao_obras.model.MarcoCronograma;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
+import java.util.List;
 
 @Repository
 public interface MarcoCronogramaRepository extends JpaRepository<MarcoCronograma , Long> {
     // Busca o cronograma associado a uma obra especifica
-    Optional<MarcoCronograma> findByCronogramaId(Long cronogramaId);
+    List<MarcoCronograma> findByCronogramaId(Long cronogramaId);
 }
